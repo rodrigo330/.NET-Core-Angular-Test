4a7c81fa8782b8fa748b14b7c1a880cd404bd192
Rodrigo Jundy Asano <rodrigojundyasano44@hotmail.com>
2019-05-31 16:10:38 -0300

    Added NGX-Bootstrap

--- web-spa/src/app/nav/nav.component.ts
+++ web-spa/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -24,11 +24,10 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
-  loggedOut() {
+  logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
   }
